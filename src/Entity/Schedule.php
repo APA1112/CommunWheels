b102597e5c9637f24dba035385a2d18880f73a7f
Alejandro <apatara1112@g.educaand.es>
2024-04-26 12:07:38 +0200

    Creating Schedule entity and repository

---
+++ src/Entity/Schedule.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScheduleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScheduleRepository::class)]
+class Schedule
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $weekDay = null;
+
+    #[ORM\Column]
+    private ?int $entrySlot = null;
+
+    #[ORM\Column]
+    private ?int $exitSlot = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWeekDay(): ?string
+    {
+        return $this->weekDay;
+    }
+
+    public function setWeekDay(?string $weekDay): Schedule
+    {
+        $this->weekDay = $weekDay;
+        return $this;
+    }
+
+    public function getEntrySlot(): ?int
+    {
+        return $this->entrySlot;
+    }
+
+    public function setEntrySlot(?int $entrySlot): Schedule
+    {
+        $this->entrySlot = $entrySlot;
+        return $this;
+    }
+
+    public function getExitSlot(): ?int
+    {
+        return $this->exitSlot;
+    }
+
+    public function setExitSlot(?int $exitSlot): Schedule
+    {
+        $this->exitSlot = $exitSlot;
+        return $this;
+    }
+
+}
