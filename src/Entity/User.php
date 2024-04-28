459c8dd9aa2741eaa7c4e79dc61e07bb967261ba
APATARA1112 <alejandropatonaraujo2@gmail.com>
2024-04-28 16:48:49 +0200

    User entity and migration

---
+++ src/Entity/User.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?bool $isAdmin = null;
+
+    #[ORM\Column]
+    private ?bool $isGroupAdmin = null;
+
+    #[ORM\Column]
+    private ?bool $isDriver = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function isIsAdmin(): ?bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(bool $isAdmin): static
+    {
+        $this->isAdmin = $isAdmin;
+
+        return $this;
+    }
+
+    public function isIsGroupAdmin(): ?bool
+    {
+        return $this->isGroupAdmin;
+    }
+
+    public function setIsGroupAdmin(bool $isGroupAdmin): static
+    {
+        $this->isGroupAdmin = $isGroupAdmin;
+
+        return $this;
+    }
+
+    public function isIsDriver(): ?bool
+    {
+        return $this->isDriver;
+    }
+
+    public function setIsDriver(bool $isDriver): static
+    {
+        $this->isDriver = $isDriver;
+
+        return $this;
+    }
+}
