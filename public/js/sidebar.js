33c45b67eaad49cae020e7ab9f7e2b8aea3f861d
APATARA1112 <alejandropatonaraujo2@gmail.com>
2024-05-23 23:35:57 +0200

    New Navbar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,6 +4,8 @@ function main(){
     const sidebar = document.querySelector('.sidebar');
     const openSidebar = document.querySelector('.navbar .menuButton a');
     const closeSidebar = document.querySelector('.sidebar li:first-child a');
+    const userDropdown = document.getElementById('userDropdown');
+    const dropdownContent = document.querySelector('.dropdown-content');
 
     // Asegurarse de que el sidebar esté oculto al cargar la página
     sidebar.style.display = 'none';
@@ -16,5 +18,21 @@ function main(){
     closeSidebar.addEventListener('click', function (event) {
         event.preventDefault();
         sidebar.style.display = 'none';
+    });
+
+    userDropdown.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (dropdownContent.style.maxHeight === '0px' || dropdownContent.style.maxHeight === '') {
+            dropdownContent.style.maxHeight = '200px'; // Ajusta esto según el contenido del menú
+        } else {
+            dropdownContent.style.maxHeight = '0px';
+        }
+    });
+
+    // Cierra el menú desplegable si se hace clic en cualquier otro lugar de la página
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('#userDropdown')) {
+            dropdownContent.style.maxHeight = '0px';
+        }
     });
 }
