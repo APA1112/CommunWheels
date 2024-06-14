ac208dceb901f823d965caadc4521e888d5cfdcd
Alejandro <alejandropatonaraujo2@gmail.com>
2024-06-14 11:55:29 +0200

    Improbing main screen

--- src/Controller/DriverController.php
+++ src/Controller/DriverController.php
@@ -38,7 +38,7 @@ class DriverController extends AbstractController
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            8 /*limit per page*/
+            9 /*limit per page*/
         );
         return $this->render('users/main.html.twig', [
             'drivers' => $drivers,
