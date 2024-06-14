4bf4de87b1bdbbca01f4dd4543d984a36a0be087
Alejandro <alejandropatonaraujo2@gmail.com>
2024-06-14 13:12:14 +0200

    sidebar dropdown

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,8 +4,8 @@ function main(){
     const sidebar = document.querySelector('.sidebar');
     const openSidebar = document.querySelector('.navbar .menuButton a');
     const closeSidebar = document.querySelector('.sidebar li:first-child a');
-    const userDropdown = document.getElementById('userDropdown');
-    const dropdownContent = document.querySelector('.dropdown-content');
+    const userDropdowns = document.querySelectorAll('.userDropdown');
+    const dropdownContents = document.querySelectorAll('.dropdown-content');
     const currentPath = window.location.pathname;
     const links = document.querySelectorAll('.navbar a');
 
@@ -22,25 +22,32 @@ function main(){
         sidebar.style.display = 'none';
     });
 
-    userDropdown.addEventListener('click', function(event) {
-        event.preventDefault();
-        if (dropdownContent.style.maxHeight === '0px' || dropdownContent.style.maxHeight === '') {
-            dropdownContent.style.maxHeight = '200px'; // Ajusta esto según el contenido del menú
-        } else {
-            dropdownContent.style.maxHeight = '0px';
-        }
+    userDropdowns.forEach((dropdown, index) => {
+        dropdown.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation(); // Detiene la propagación del evento
+            const dropdownContent = dropdownContents[index];
+            const isOpen = dropdownContent.style.maxHeight === '200px';
+            dropdownContent.style.maxHeight = isOpen ? '0px' : '200px';
+            dropdown.classList.toggle('open', !isOpen); // Alternar la clase open
+        });
     });
 
     // Cierra el menú desplegable si se hace clic en cualquier otro lugar de la página
     window.addEventListener('click', function(event) {
-        if (!event.target.matches('#userDropdown')) {
-            dropdownContent.style.maxHeight = '0px';
-        }
+        userDropdowns.forEach((dropdown, index) => {
+            const dropdownContent = dropdownContents[index];
+            if (!event.target.closest('.userDropdown')) {
+                dropdownContent.style.maxHeight = '0px';
+                dropdown.classList.remove('open'); // Asegurarse de que el icono vuelva a su posición original
+            }
+        });
     });
 
     // Añadir clase 'active' solo a los enlaces de Grupos, Usuarios y Notificaciones
     links.forEach(link => {
-        if (link.href.includes(currentPath) && (link.href.includes('group_main') || link.href.includes('driver_main') || link.href.includes('notify_main'))) {
+        const linkPath = new URL(link.href).pathname;
+        if (linkPath === currentPath && (linkPath.includes('group_main') || linkPath.includes('driver_main') || linkPath.includes('notify_main'))) {
             link.classList.add('active');
         }
     });
